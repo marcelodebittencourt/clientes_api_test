0ee7aecb52a2a08f25aa59e97bd2023acfaf9f90
Marcelo Bittencourt <tchelopoa@gmail.com>
2021-12-04 16:35:57 -0300

    Refatoração para usar as variáveis de base do RestAssured

--- src/test/java/localhost/ClientsTest.java
+++ src/test/java/localhost/ClientsTest.java
@@ -1,17 +1,24 @@
 package localhost;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.Matchers.*;
 
 public class ClientsTest {
+
+    @BeforeAll
+    static void setup() {
+        RestAssured.baseURI = "http://localhost:8080";
+        RestAssured.basePath = "/clients";
+    }
 
     @Test
     void testListaClientes_TodosClientesSaoListados() {
         RestAssured.given() //Dado - Given
                 .log().all() //loga toda a request - solicitação (entrada) - mostrará o cabeçalho da solicitação
                 .when() //Quando - When
-                .get("http://localhost:8080/clients") //Faz a solicitação em si, método/verbo GET passando como parâmetro a URL/URI a ser acessada
+                .get() //Faz a solicitação em si, método/verbo GET passando como parâmetro a URL/URI a ser acessada
                 .then() //Então
                 .log().all()  // loga toda a response - resposta
                 .statusCode(200);  //verifica se o resultado da request é HTTP 200 OK
@@ -22,7 +29,7 @@ public class ClientsTest {
         RestAssured.given() //Dado - Given
                 .log().all() //loga toda a request - solicitação (entrada) - mostrará o cabeçalho da solicitação
                 .when() //Quando - When - REQUEST
-                .get("http://localhost:8080/clients/1") //Faz a solicitação em si, método/verbo GET passando como parâmetro a URL/URI a ser acessada
+                .get("/1") //Faz a solicitação em si, método/verbo GET passando como parâmetro a URL/URI a ser acessada
                 .then() //Então - RESPONSE
                 .log().all()  // loga toda a response - resposta
                 .statusCode(200)  //verifica se o resultado da request é HTTP 200 OK
