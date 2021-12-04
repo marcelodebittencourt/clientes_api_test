f7a5a55db298c8803cb0cfa7712cb78e0062d91c
Marcelo Bittencourt <tchelopoa@gmail.com>
2021-12-04 16:55:33 -0300

    Incluir POST e TODO para os métodos restantes

--- src/test/java/localhost/ClientsTest.java
+++ src/test/java/localhost/ClientsTest.java
@@ -1,6 +1,7 @@
 package localhost;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.Matchers.*;
@@ -39,5 +40,26 @@ public class ClientsTest {
                 .body("id", is(id));
     }
 
+    @Test
+    void testIncluiCliente_ClienteEhIncluidoComSucesso() {
+
+        String email = "fulana@teste.com";
+        String name = "Fulana";
+
+        RestAssured.given() //Dado - Given - Pré-condição
+                .body("{\n" +
+                        "  \"email\": \"" + email + "\",\n" +
+                        "  \"name\": \"" + name + "\" \n" +
+                        "}")
+                .log().all() //loga toda a request - solicitação (entrada) - mostrará o cabeçalho da solicitação
+                .contentType(ContentType.JSON)
+                .when() //Quando - When - REQUEST
+                .post() //Faz a solicitação em si, método/verbo GET passando como parâmetro a URL/URI a ser acessada
+                .then() //Então - RESPONSE
+                .log().all()  // loga toda a response - resposta
+                .statusCode(201);  //verifica se o resultado da request é HTTP 200 OK
+    }
+
+    //TODO fazer testes para os verbos DELETE e PUT
 
 }
