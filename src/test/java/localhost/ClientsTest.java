fd31d36179233bb8879a493ea419d3139f96126b
Marcelo Bittencourt <tchelopoa@gmail.com>
2021-12-11 11:21:42 -0300

    Ajuste de código proposto pelo SonarLint

--- src/test/java/localhost/ClientsTest.java
+++ src/test/java/localhost/ClientsTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.Matchers.*;
 
-public class ClientsTest {
+class ClientsTest {
 
     @BeforeAll
     static void setup() {
